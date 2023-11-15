2b44e67f4530d27064fccafd4e3bee6d31ba7c61
locdang9 <dang.baloc@hachi-x.com>
2023-11-15 18:47:31 +0900

    updated dependencies for imu fusion

--- src/nitrabot_dockerfiles/install/i_dep.sh
+++ src/nitrabot_dockerfiles/install/i_dep.sh
@@ -15,7 +15,9 @@ ros_dep="ros-noetic-tf \
          ros-noetic-nmea-navsat-driver \
          ros-noetic-xacro \
          ros-noetic-ros-control \
-         ros-noetic-ros-controllers"
+         ros-noetic-ros-controllers \
+         ros-noetic-rviz-imu-plugin \
+         ros-noetic-imu-filter-madgwick"
 
 python_dep="rospy-message-converter \
          python-socketio
